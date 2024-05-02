4b36483f14c7480875e421c81bd43149d13f6930
Moeein Aali <moeeeinaali@gmail.com>
2024-05-02 22:07:45 +0330

    Add Session06 Solution

---
+++ Assignments/08-SQL in Industry/شیپور/queries.sql
@@ -0,0 +1,12 @@
+-- Section1
+    your 1st query here
+-- Section2
+    your 2nd query here
+-- Section3
+    your 3rd query here
+-- Section4
+    your 4rd query here
+-- Section5
+    your 5rd query here
+-- Section6
+    your 6rd query here
